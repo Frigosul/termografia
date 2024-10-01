49ba457a184a80cb078bc10e2ade682e1fdb7698
João Dias <joaoeduardodias123@gmail.com>
2024-10-01 11:09:37 -0300

    feat: add responsive page login

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "Login | Termografia",
-
-};
-export default function AuthLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  title: 'Login | Termografia',
+}
+export default function AuthLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <main className="flex justify-center items-center h-screen">
+    <main className="flex flex-col lg:flex-row justify-center items-center h-screen">
       {children}
     </main>
-  );
+  )
 }
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -6,15 +6,20 @@ import { SignInForm } from './components/sign-in-form'
 export default function SignIn() {
   return (
     <>
-      <div className="flex justify-center items-center border-r  w-full h-full">
+      <div className="flex justify-center items-center border-r  w-screen h-screen">
         <div className="flex flex-col space-y-2">
-          <Image src={logo} width={280} alt="Logo frigosul" />
-          <h1 className="text-center text-3xl font-semibold tracking-tight">
+          <Image
+            src={logo}
+            className="w-60 lg:w-72"
+            alt="Logo frigosul"
+            priority
+          />
+          <h1 className="text-center text-xl lg:text-3xl font-semibold tracking-tight">
             Termografia
           </h1>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center   w-full h-full">
+      <div className="flex flex-col justify-center items-center w-full h-full mb-20 lg:mb-0">
         <div className="flex flex-col gap-2 text-center">
           <h2 className="text-2xl font-semibold tracking-tight mb-5">
             Acessar painel
