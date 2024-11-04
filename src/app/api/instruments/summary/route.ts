8e0f2b99c7c6aaba557515d98af2c947d31e58a7
João Dias <joaoeduardodias123@gmail.com>
2024-11-04 15:19:32 -0300

    refactor: alter route summary

--- src/app/api/instruments/summary/route.ts
+++ src/app/api/instruments/summary/route.ts
@@ -9,47 +9,17 @@ export async function GET() {
       id: true,
       name: true,
       type: true,
-      status: true,
-      error: true,
       minValue: true,
       maxValue: true,
-      isSensorError: true,
-      temperatures: {
-        select: {
-          temperature: {
-            select: {
-              editValue: true,
-              createdAt: true,
-            }
+      isActiive: true,
+      displayOrder: true,
 
-          },
-        },
-        orderBy: {
-          temperature: {
-            updatedAt: 'desc'
-          },
-
-        },
-        take: 1
-      }
     },
     orderBy: {
       name: 'asc'
     }
   })
 
-  const formattedInstruments = instruments.map(instrument => ({
-    id: instrument.id,
-    name: instrument.name,
-    type: instrument.type,
-    status: instrument.status,
-    isSensorError: instrument.isSensorError,
-    temperature: instrument.temperatures?.[0]?.temperature?.editValue ?? null,
-    createdAt: instrument.temperatures?.[0].temperature.createdAt,
-    error: instrument.error,
-    maxValue: instrument.maxValue,
-    minValue: instrument.minValue,
-  }));
 
-  return NextResponse.json(formattedInstruments, { status: 200 })
+  return NextResponse.json(instruments, { status: 200 })
 }
