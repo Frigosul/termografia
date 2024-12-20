bd42b56c8163e227011421f62c399f9c1f160c06
João Dias <joaoeduardodias123@gmail.com>
2024-12-20 07:46:21 -0300

    refactor: alter skeleton table

--- src/app/(home)/users/components/skeleton-table.tsx
+++ src/app/(home)/users/components/skeleton-table.tsx
@@ -5,19 +5,22 @@ export interface SkeletonTableProps { }
 
 export function SkeletonTable(props: SkeletonTableProps) {
 
-  return Array.from({ length: 4 }).map((_, index) => (
+  return Array.from({ length: 3 }).map((_, index) => (
     <TableRow key={index}>
       <TableCell className="border min-w-52 w-96">
-        <Skeleton className="h-4" />
+        <Skeleton className="h-6" />
       </TableCell>
       <TableCell className="border min-w-52 w-96">
-        <Skeleton className="h-4" />
+        <Skeleton className="h-6" />
       </TableCell>
       <TableCell className="border min-w-40">
-        <Skeleton className="h-4" />
+        <Skeleton className="h-6" />
       </TableCell>
       <TableCell className=" w-4 border">
-        <Skeleton className="h-4" />
+        <Skeleton className="h-6" />
+      </TableCell>
+      <TableCell className=" w-4 border">
+        <Skeleton className="h-6" />
       </TableCell>
     </TableRow>
   ))
