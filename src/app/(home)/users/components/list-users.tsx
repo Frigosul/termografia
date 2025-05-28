cc1639cae5a28c460f71c7aa8a6987729b18a28a
João Dias <joaoeduardodias123@gmail.com>
2025-05-28 14:57:40 -0300

    fix: alter spinner

--- src/app/(home)/users/components/list-users.tsx
+++ src/app/(home)/users/components/list-users.tsx
@@ -27,6 +27,8 @@ export function ListUsers() {
     staleTime: 1000 * 60 * 60, // 1 hour
   })
 
+  if (error) return <p>Erro ao carregar usuários.</p>
+  if (isLoading) return <Spinner size={62} className="mx-auto my-20" />
   return (
     <Table className="max-h-[70vh] rounded-md max-w-screen-2xl">
       <TableHeader>
@@ -39,60 +41,52 @@ export function ListUsers() {
         </TableRow>
       </TableHeader>
       <TableBody className="overflow-y-auto">
-        {isLoading ? (
-          <div className="flex items-center justify-center h-[calc(100vh-200px)] w-full">
-            <Spinner size={62} />
-          </div>
-        ) : error ? (
-          <p>Erro encontrado, tente novamente.</p>
-        ) : (
-          users?.map((user) => {
-            return (
-              <TableRow
-                key={user.id}
-                className="odd:bg-white odd:dark:bg-slate-950 even:bg-slate-50 even:dark:bg-slate-900"
-              >
-                <TableCell className="border min-w-52 w-96">
-                  {user.name}
-                </TableCell>
-                <TableCell className="border min-w-52 w-96">
-                  {user.email}
-                </TableCell>
-                <TableCell className="border min-w-40">
-                  {userRoles[user.userRole] || user.userRole}
-                </TableCell>
-                <TableCell className="text-center border p-0 w-16">
-                  <Button
-                    variant="ghost"
-                    className="p-0 m-0"
-                    onClick={() =>
-                      openModal('update-user', {
-                        id: String(user.id),
-                        email: String(user.email),
-                        name: String(user.name),
-                        password: user.password,
-                        role: user.userRole as UserRolesType,
-                      })
-                    }
-                  >
-                    <Pencil className="size-4" />
-                  </Button>
-                </TableCell>
-                <TableCell className="text-center  border px-2 py-0 w-16">
-                  <Button
-                    variant="ghost"
-                    className="p-0 m-0"
-                    onClick={() => {
-                      openModal('delete-user', { id: String(user.id) })
-                    }}
-                  >
-                    <Trash2 className="size-4" />
-                  </Button>
-                </TableCell>
-              </TableRow>
-            )
-          })
-        )}
+        {users?.map((user) => {
+          return (
+            <TableRow
+              key={user.id}
+              className="odd:bg-white odd:dark:bg-slate-950 even:bg-slate-50 even:dark:bg-slate-900"
+            >
+              <TableCell className="border min-w-52 w-96">
+                {user.name}
+              </TableCell>
+              <TableCell className="border min-w-52 w-96">
+                {user.email}
+              </TableCell>
+              <TableCell className="border min-w-40">
+                {userRoles[user.userRole] || user.userRole}
+              </TableCell>
+              <TableCell className="text-center border p-0 w-16">
+                <Button
+                  variant="ghost"
+                  className="p-0 m-0"
+                  onClick={() =>
+                    openModal('update-user', {
+                      id: String(user.id),
+                      email: String(user.email),
+                      name: String(user.name),
+                      password: user.password,
+                      role: user.userRole as UserRolesType,
+                    })
+                  }
+                >
+                  <Pencil className="size-4" />
+                </Button>
+              </TableCell>
+              <TableCell className="text-center  border px-2 py-0 w-16">
+                <Button
+                  variant="ghost"
+                  className="p-0 m-0"
+                  onClick={() => {
+                    openModal('delete-user', { id: String(user.id) })
+                  }}
+                >
+                  <Trash2 className="size-4" />
+                </Button>
+              </TableCell>
+            </TableRow>
+          )
+        })}
       </TableBody>
     </Table>
   )
